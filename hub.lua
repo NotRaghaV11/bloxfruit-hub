8371ecba31cd41a9a486bbfef281c3c868b0c93e
NotXd <162581621+NotRaghaV11@users.noreply.github.com>
2025-07-27 22:49:14 +0530

    hub.lua

--- hub.lua
+++ hub.lua
@@ -1,1 +1,13 @@
 
+local scripts = {
+    "autofarm.lua",
+    "fruitfinder.lua",
+    "esp.lua",
+    "teleport.lua",
+    "bossfarm.lua",
+    "autostats.lua"
+}
+
+for _, script in pairs(scripts) do
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NotRaghaV11/bloxfruit-hub/main/" .. script))()
+end
